9deec68d4801f836aa69125e1a911ab2d205b026
espern <loic.espern@gmail.com>
2013-05-14 10:13:23 +0200

    fixed error in menu (no files to download) and files-list display without component

--- models/menu.py
+++ models/menu.py
@@ -37,7 +37,7 @@ images = db(db.image).select()
 if images:
 	response.menu += [(T('Photo gallery'), False, URL('images','images'))]
 
-files = db(db.file.page==None).select()
+files = db(db.file.page<1).select()
 if files or auth.has_membership('manager'):
 	response.menu += [(T('Files to download'), False, URL('files','files_list'))]
 
